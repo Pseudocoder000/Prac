f5f0004016eaa05e2cc598d3144da11af7aca85a
Pseudocoder <110370085+Pseudocoder000@users.noreply.github.com>
2026-06-30 00:05:21 +0530

    Time: 37 ms (97.07%), Space: 70.9 MB (93.29%) - LeetHub

---
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reductionOperations(int[] nums) {
+        Arrays.sort(nums);
+
+        int ans = 0;
+        int count = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                count++;
+            }
+            ans += count;
+        }
+
+        return ans;
+    }
+}
